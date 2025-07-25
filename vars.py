05b3a58cb9f6bfd6a4981c080caf296442a984e7
Sevmamra <sevmamra01@gmail.com>
2025-07-25 10:51:19 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -8,7 +8,7 @@ API_HASH = environ.get("API_HASH", "9699262c708c2e45ba18bfce925ed5ed")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 OWNER = int(environ.get("OWNER", "6567162029"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+CREDIT = environ.get("CREDIT", "—͟͞͞𝙎𝖆𝖚𝖗𝖆ꪜˢⁱⁿᵍʰ")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
